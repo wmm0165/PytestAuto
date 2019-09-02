d411eb51a0ff2fc1e01d3b0f5aa58a7123a387ea
wangmengmeng <wmm_0165@163.com>
2019-09-02 17:43:36 +0800

    Initial commit

---
+++ data/login_data.py
@@ -0,0 +1,32 @@
+class LoginData(object):
+    """用户登录测试数据"""
+
+    login_success_data = [
+        (
+            "admin",
+            "123456",
+            "linuxxiaochao@126.com"
+        )
+    ]
+
+    login_fail_data = [
+        (
+            "linuxxiaochao",
+            "",
+            "请输入密码"
+        ),
+        (
+            "",
+            "xiaochao11520",
+            "请输入帐号"
+        ),
+        (
+            "linux",
+            "xiaochao",
+            "帐号或密码错误"
+        )
+    ]
+
+
+# if __name__ == '__main__':
+#     pass
